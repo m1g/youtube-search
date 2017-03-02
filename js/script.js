a9a51ccf84ba4aa45f282c5ee9e755daeabdc789
m1g <miguelmalcolm@outlook.com>
2017-03-02 10:37:33 -0500

    implemented youtube api

--- js/script.js
+++ js/script.js
@@ -25,4 +25,40 @@ $(() => {
       }, 400, function() {})
     }
   })
+
+  $('#search-form').submit(function(e){
+    e.preventDefault()
+  })
  })
+
+ function search() {
+   // clear results
+   $('results').html('')
+   $('buttons').html('')
+
+   // Get From Input
+   q = $('#guery').val()
+
+   // Run GET request on API
+   $.get(
+     "https://www.googleapis.com/youtube/v3/search", {
+       part: 'snippet, id',
+       q: q,
+       type: 'video',
+       key: 'AIzaSyDT8lXVfqcbXH5nLrgeotrBuRO2iD4-ZKU'},
+       function(data) {
+         var nextPageToken = data.nextPageToken
+         var prevPageToken = data.prevPageToken
+
+         console.log(data)
+
+         $.each(data.items, function(i, item) { // Custom Function for Data Output
+           // Get Output
+           var output = getOutput(item)
+
+           // Display Results
+           $('#results').append(output)
+         })
+       }
+   )
+ }
