c190bae945d7681a5691d1e27165727679335d61
m1g <miguelmalcolm@outlook.com>
2017-03-07 20:39:46 -0500

    error code 403. API issue

--- js/script.js
+++ js/script.js
@@ -1,99 +1,187 @@
 // Searchbar Handler
+$(function(){
+	var searchField = $('#query');
+	var icon = $('#search-btn');
 
-$(() => {
-  const searchField = $('#query');
-  const icon = $('#search-btn');
+	// Focus Event Handler
+	$(searchField).on('focus', function(){
+		$(this).animate({
+			width:'100%'
+		},400);
+		$(icon).animate({
+			right: '10px'
+		}, 400);
+	});
 
-  // Focus Event Handler
-  $(searchField).on('focus', function(){
-    $(this).animate({
-      width: '100%'
-    }, 400)
-    $(icon).animate({
-      right: '10px'
-    }, 400)
-  })
+	// Blur Event Handler
+	$(searchField).on('blur', function(){
+		if(searchField.val() == ''){
+			$(searchField).animate({
+				width:'45%'
+			},400, function(){});
+			$(icon).animate({
+				right:'360px'
+			},400, function(){});
+		}
+	});
 
-  // Blur Event Handler
-  $(searchField).on('blur', function(){
-    if (searchField.val() == '') {
-      $(searchField).animate({
-        width: '45%'
-      }, 400, function() {})
-      $(icon).animate({
-        right: '360px'
-      }, 400, function() {})
-    }
-  })
+	$('#search-form').submit(function(e){
+		e.preventDefault();
+	});
+})
 
-  $('#search-form').submit(function(e){
-    e.preventDefault()
-  })
- })
 
- const search = () => {
-   // clear results
-   $('results').html('')
-   $('buttons').html('')
+function search(){
+	// Clear Results
+	$('#results').html('');
+	$('#buttons').html('');
 
-   // Get From Input
-   q = $('#guery').val()
+	// Get Form Input
+	q = $('#query').val();
 
-   // Run GET request on API
-   $.get(
-     "https://www.googleapis.com/youtube/v3/search", {
-       part: 'snippet, id',
-       q: q,
-       type: 'video',
-       key: 'AIzaSyDT8lXVfqcbXH5nLrgeotrBuRO2iD4-ZKU'},
-       function(data) {
-         var nextPageToken = data.nextPageToken
-         var prevPageToken = data.prevPageToken
+	// Run GET Request on API
+	$.get(
+		"https://www.googleapis.com/youtube/v3/search",{
+			part: 'snippet, id',
+			q: q,
+			type:'video',
+			key: 'AIzaSyCvk3NNMQASZgFkCNxIp9jH-l8O0PXhDUo'},
+			function(data){
+				var nextPageToken = data.nextPageToken;
+				var prevPageToken = data.prevPageToken;
 
-         console.log(data)
+				// Log Data
+				console.log(data);
 
-         $.each(data.items, (i, item) => { // Custom Function for Data Output
-           // Get Output
-           let output = getOutput(item)
+				$.each(data.items, function(i, item){
+					// Get Output
+					var output = getOutput(item);
 
-           // Display Results
-           $('#results').append(output)
-         })
+					// Display Results
+					$('#results').append(output);
+				});
 
-         var buttons = getButtons(prevPageToken, nextPageToken)
+				var buttons = getButtons(prevPageToken, nextPageToken);
 
-         // Display Buttons
- 				$('#buttons').append(buttons);
- 			}
- 	);
- }
+				// Display Buttons
+				$('#buttons').append(buttons);
+			}
+	);
+}
 
- // Build Output
- function getOutput(item){
- 	var videoId = item.id.videoId;
- 	var title = item.snippet.title;
- 	var description = item.snippet.description;
- 	var thumb = item.snippet.thumbnails.high.url;
- 	var channelTitle = item.snippet.channelTitle;
- 	var videoDate = item.snippet.publishedAt;
+// Next Page Function
+function nextPage(){
+	var token = $('#next-button').data('token');
+	var q = $('#next-button').data('query');
 
- 	// Build Output String
- 	var output = '<li>' +
- 	'<div class="list-left">' +
- 	'<img src="'+thumb+'">' +
- 	'</div>' +
- 	'<div class="list-right">' +
- 	'<h3>'+title+'</h3>' +
- 	'<small>By <span class="cTitle">'+channelTitle+'</span> on '+videoDate+'</small>' +
- 	'<p>'+description+'</p>' +
- 	'</div>' +
- 	'</li>' +
- 	'<div class="clearfix"></div>' +
- 	'';
+	// Clear Results
+	$('#results').html('');
+	$('#buttons').html('');
 
- 	return output;
- }
- 
+	// Get Form Input
+	q = $('#query').val();
+
+	// Run GET Request on API
+	$.get(
+		"https://www.googleapis.com/youtube/v3/search",{
+			part: 'snippet, id',
+			q: q,
+			pageToken: token,
+			type:'video',
+			key: 'AIzaSyDT8lXVfqcbXH5nLrgeotrBuRO2iD4-ZKU'},
+			function(data){
+				var nextPageToken = data.nextPageToken;
+				var prevPageToken = data.prevPageToken;
+
+				// Log Data
+				console.log(data);
+
+				$.each(data.items, function(i, item){
+					// Get Output
+					var output = getOutput(item);
+
+					// Display Results
+					$('#results').append(output);
+				});
+
+				var buttons = getButtons(prevPageToken, nextPageToken);
+
+				// Display Buttons
+				$('#buttons').append(buttons);
+			}
+	);
+}
+
+
+// Prev Page Function
+function prevPage(){
+	var token = $('#prev-button').data('token');
+	var q = $('#prev-button').data('query');
+
+	// Clear Results
+	$('#results').html('');
+	$('#buttons').html('');
+
+	// Get Form Input
+	q = $('#query').val();
+
+	// Run GET Request on API
+	$.get(
+		"https://www.googleapis.com/youtube/v3/search",{
+			part: 'snippet, id',
+			q: q,
+			pageToken: token,
+			type:'video',
+			key: 'AIzaSyCvk3NNMQASZgFkCNxIp9jH-l8O0PXhDUo'},
+			function(data){
+				var nextPageToken = data.nextPageToken;
+				var prevPageToken = data.prevPageToken;
+
+				// Log Data
+				console.log(data);
+
+				$.each(data.items, function(i, item){
+					// Get Output
+					var output = getOutput(item);
+
+					// Display Results
+					$('#results').append(output);
+				});
+
+				var buttons = getButtons(prevPageToken, nextPageToken);
+
+				// Display Buttons
+				$('#buttons').append(buttons);
+			}
+	);
+}
+
+// Build Output
+function getOutput(item){
+	var videoId = item.id.videoId;
+	var title = item.snippet.title;
+	var description = item.snippet.description;
+	var thumb = item.snippet.thumbnails.high.url;
+	var channelTitle = item.snippet.channelTitle;
+	var videoDate = item.snippet.publishedAt;
+
+	// Build Output String
+	var output = '<li>' +
+	'<div class="list-left">' +
+	'<img src="'+thumb+'">' +
+	'</div>' +
+	'<div class="list-right">' +
+	'<h3><a class="fancybox fancybox.iframe" href="http://www.youtube.com/embed/'+videoId+'">'+title+'</a></h3>' +
+	'<small>By <span class="cTitle">'+channelTitle+'</span> on '+videoDate+'</small>' +
+	'<p>'+description+'</p>' +
+	'</div>' +
+	'</li>' +
+	'<div class="clearfix"></div>' +
+	'';
+
+	return output;
+}
+
 // Build the buttons
 function getButtons(prevPageToken, nextPageToken){
 	if(!prevPageToken){
